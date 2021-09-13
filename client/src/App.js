56ae687df28b02305a1ddd34e547a132d6fa2a61
Joey Davidson <j.davidson18@gmail.com>
2021-09-13 06:33:30 -0600

    BrowserRouter up and running, Pages and Recipe pages navigating properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home/Home";
+import Recipe from "./pages/Recipe/Recipe";
 
 function App() {
-  return <>Hello From App</>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={["/", "/home"]}>
+          <Home />
+        </Route>
+        <Route exact path={"/recipe"}>
+          <Recipe />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
