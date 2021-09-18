605bfb425e46af935f34a8adf0707984ef2884d5
Joey Davidson <j.davidson18@gmail.com>
2021-09-18 12:10:27 -0600

    conditional input algorithm for base query functioning

---
+++ client/src/components/QueryFunction.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function QueryFunction() {
+  {
+    if (recipeSearch.current.value === "") {
+      console.log("it is undefined and should be blank");
+    } else {
+      console.log("it is not null and is: " + recipeSearch);
+    }
+  }
+
+  return <></>;
+}
--- client/src/components/SearchForm/SearchForm.js
+++ client/src/components/SearchForm/SearchForm.js
@@ -15,39 +15,102 @@ export default function SearchForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("recipeSearch current below");
-    console.log(recipeSearch);
+    let recipeInput = recipeSearch.current.value;
+    let bookInput = bookSearch.current.value;
+    let ingredInput = ingredSearch.current.value;
 
     // API.getRecipes().then((data) => {
     //   console.log(data);
     // });
 
-    // typeof recipeSearch !== null
-    //   ? console.log("it is not undefined and is " + recipeSearch)
-    //   : console.log("it is undefined and should be blank");
+    // API.findByBook(bookSearch.current.value).then((data) => {
+    //   console.log(data);
+    //   dispatch({
+    //     type: SET_CURRENT_BOOK,
+    //     book: data.data[0],
+    //   });
+    //   console.log(data.data);
+    //   console.log(state);
+    // });
 
-    recipeSearch.current.value === ""
-      ? console.log("it is undefined and should be blank")
-      : console.log("it is not null and is: " + recipeSearch);
+    // API.findByRecipe(recipeSearch.current.value).then((data) => {
+    //   dispatch({
+    //     type: SET_CURRENT_RECIPE,
+    //     recipe: data.data[0],
+    //   });
+    // });
+
+    // recipeSearch.current.value === ""
+    //   ? console.log("it is undefined and should be blank")
+    //   : console.log("it is not null and is: " + recipeSearch);
+
+    // if (recipeInput === "") {
+    //   console.log("it is undefined and should be blank");
+    // } else {
+    //   console.log("it is not null and is: " + recipeSearch);
+    // }
+
+    // recipeInput === ""
+    // ? bookInput === ""
+    //   ? ingredInput === ""
+    //     ?
+    //     :
+    //   :
+    // : bookInput === ""
+    //   ? console.log('recipeInput has something but bookInput is empty')
+    //   : ingredInput === ""
+    //     ? console.log('THIS IS R + B')
+    //     : console.log('THIS IS R + B + I')
+
+    recipeInput === ""
+      ? console.log("recipeInput is empty")
+      : bookInput === ""
+      ? ingredInput === ""
+        ? console.log("this is R only")
+        : console.log("this is R + I")
+      : ingredInput === ""
+      ? console.log("THIS IS R + B")
+      : console.log("THIS IS R + B + I");
+
+    bookInput === ""
+      ? console.log("bookInput is empty")
+      : ingredInput === ""
+      ? recipeInput === ""
+        ? console.log("this is B only")
+        : console.log("this is B + R")
+      : recipeInput === ""
+      ? console.log("THIS IS B + I")
+      : console.log("THIS IS R + B + I");
+
+    ingredInput === ""
+      ? console.log("ingredInput is empty")
+      : recipeInput === ""
+      ? bookInput === ""
+        ? console.log("this is I only")
+        : console.log("this is I + B")
+      : bookInput === ""
+      ? console.log("THIS IS I + R")
+      : console.log("THIS IS R + B + I");
+
+    //   {
+    //     (() => {
+    //         if (conditionOne)
+    //            return <span>One</span>
+    //         if (conditionTwo)
+    //            return <span>Two</span>
+    //         else (conditionOne)
+    //            return <span>Three</span>
+    //     })()
+    //  }
+
+    // if (recipeInput && bookInput && ingredInput === "") {
+    //   alert("Please enter at least one input field!");
+    // } else {
+    //   console.log(
+    //     "it is not null and is: " + recipeSearch + bookSearch + ingredSearch
+    //   );
+    // }
   };
-  function getRecipe() {
-    API.findByRecipe(recipeSearch.current.value).then((data) => {
-      dispatch({
-        type: SET_CURRENT_RECIPE,
-        recipe: data.data[0],
-      });
-    });
-  }
-
-  // API.findByBook(bookSearch.current.value).then((data) => {
-  //   console.log(data);
-  //   dispatch({
-  //     type: SET_CURRENT_BOOK,
-  //     book: data.data[0],
-  //   });
-  //   console.log(data.data);
-  //   console.log(state);
-  // });
 
   return (
     <>
