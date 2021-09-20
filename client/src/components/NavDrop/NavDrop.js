823b51108bad1594c9bd49ab2b44d47ca4ac93f5
Joey Davidson <j.davidson18@gmail.com>
2021-09-20 13:59:31 -0600

    book titles moved to Global State, dropdown mapping off of this now

--- client/src/components/NavDrop/NavDrop.js
+++ client/src/components/NavDrop/NavDrop.js
@@ -1,34 +1,12 @@
 import React, { useEffect } from "react";
 import { NavDropdown } from "react-bootstrap";
 import API from "../../utils/API";
-
-const allBooks = [
-  "Catering to Nobody",
-  "Dying for Chocolate",
-  "The Cereal Murders",
-  "The Last Suppers",
-  "Killer Pancake",
-  "The Main Corpse",
-  "The Grilling Season",
-  "Prime Cut",
-  "Tough Cookie",
-  "Sticks and Scones",
-  "Chopping Spree",
-  "Double Shot",
-  "Dark Tort",
-  "Sweet Revenge",
-  "Fatally Flaky",
-  "Crunch Time",
-  "The Whole Enchilada",
-];
+import { useStoreContext } from "../../utils/GlobalState";
 
 export default function NavDrop() {
-  useEffect(() => {
-    API.singleQuery("test", "allBooks").then((data) => {
-      console.log("returned books below");
-      console.log(data);
-    });
-  }, []);
+  const [state, dispatch] = useStoreContext();
+
+  console.log(state);
 
   return (
     <NavDropdown
@@ -39,7 +17,7 @@ export default function NavDrop() {
       }
       id="basic-nav-dropdown"
     >
-      {allBooks.map((bookTitle) => {
+      {state.allBooks.map((bookTitle) => {
         return (
           <NavDropdown.Item href="#action/3.1">{bookTitle}</NavDropdown.Item>
         );
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -54,6 +54,25 @@ const StoreProvider = ({ value = [], ...props }) => {
     course: [],
     book: [],
     meat: "",
+    allBooks: [
+      "Catering to Nobody",
+      "Dying for Chocolate",
+      "The Cereal Murders",
+      "The Last Suppers",
+      "Killer Pancake",
+      "The Main Corpse",
+      "The Grilling Season",
+      "Prime Cut",
+      "Tough Cookie",
+      "Sticks and Scones",
+      "Chopping Spree",
+      "Double Shot",
+      "Dark Tort",
+      "Sweet Revenge",
+      "Fatally Flaky",
+      "Crunch Time",
+      "The Whole Enchilada",
+    ],
   });
 
   return <Provider value={[state, dispatch]} {...props} />;
