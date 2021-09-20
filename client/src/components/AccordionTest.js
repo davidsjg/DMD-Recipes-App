2e97368bd539d5d76e15035debd8d292526b8b43
Joey Davidson <j.davidson18@gmail.com>
2021-09-20 12:15:44 -0600

    Accordion added with buttons

--- client/src/components/AccordionTest.js
+++ client/src/components/AccordionTest.js
@@ -1,37 +1,123 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
+import { useAccordionButton } from "react-bootstrap/esm/AccordionButton";
 import Card from "react-bootstrap/Card";
+
 // var Accordion = require("react-bootstrap").Accordion;
 
-export default function AccordionTest() {
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+    console.log("totally custom!")
+  );
+
+  return (
+    <Button
+      type="button"
+      style={{ backgroundColor: "cornflowerBlue " }}
+      onClick={decoratedOnClick}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default function AccordionExample() {
   return (
     <>
       <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
+        <Card>
+          <Card.Header className="homeAccord">
+            <CustomToggle eventKey="1">Search by Ingredient</CustomToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header className="homeAccord">
+            <CustomToggle eventKey="2">Search by Ingredient</CustomToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="2">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header className="homeAccord">
+            <CustomToggle eventKey="3">Search by Book</CustomToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="3">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header className="homeAccord">
+            <CustomToggle eventKey="4">Search by Course</CustomToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="4">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
       </Accordion>
     </>
   );
 }
+
+// export default function AccordionTest() {
+//   return (
+//     <>
+//       <form>
+//         <Accordion>
+//           <Accordion.Item eventKey="0">
+//             <Accordion.Header>Recipe</Accordion.Header>
+//             <Accordion.Body>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//               enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+//               reprehenderit in voluptate velit esse cillum dolore eu fugiat
+//               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+//               sunt in culpa qui officia deserunt mollit anim id est laborum.
+//             </Accordion.Body>
+//           </Accordion.Item>
+//           <Accordion.Item eventKey="1">
+//             <Accordion.Header>Ingredient</Accordion.Header>
+//             <Accordion.Body>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//               enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+//               reprehenderit in voluptate velit esse cillum dolore eu fugiat
+//               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+//               sunt in culpa qui officia deserunt mollit anim id est laborum.
+//             </Accordion.Body>
+//           </Accordion.Item>
+//           <Accordion.Item eventKey="2">
+//             <Accordion.Header>Book</Accordion.Header>
+//             <Accordion.Body>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//               enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+//               reprehenderit in voluptate velit esse cillum dolore eu fugiat
+//               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+//               sunt in culpa qui officia deserunt mollit anim id est laborum.
+//             </Accordion.Body>
+//           </Accordion.Item>
+//           <Accordion.Item eventKey="3">
+//             <Accordion.Header>Accordion Item #2</Accordion.Header>
+//             <Accordion.Body>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//               enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+//               reprehenderit in voluptate velit esse cillum dolore eu fugiat
+//               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+//               sunt in culpa qui officia deserunt mollit anim id est laborum.
+//             </Accordion.Body>
+//           </Accordion.Item>
+//         </Accordion>
+//       </form>
+//     </>
+//   );
+// }
