80fc9240fab87184b16a4806c276d2aee8be5e38
Joey Davidson <j.davidson18@gmail.com>
2021-09-24 13:51:02 -0600

    successful API call from history

--- client/src/pages/RecipeSelect.js
+++ client/src/pages/RecipeSelect.js
@@ -1,13 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import RecipeContext from "..//utils/RecipeContext";
-import { useHistory } from "react-router-dom";
+import API from "../utils/API";
 
 export default function RecipeSelect(props) {
   const { bookRecipes, ingredRecipes, courseRecipes, currRecipes } =
     useContext(RecipeContext);
 
   console.log("currRecipe below");
-  console.log(props);
+  console.log(props.match.params.recipe);
+  let historyRecipe = props.match.params.recipe;
+
+  useEffect(() => {
+    API.singleQuery(historyRecipe, "recipe").then((data) => {
+      // setIngredRecipes([data.data]);
+      console.log("return from history search");
+      console.log(data.data);
+    });
+  }, []);
 
   return "hello from Recipe Select";
 }
