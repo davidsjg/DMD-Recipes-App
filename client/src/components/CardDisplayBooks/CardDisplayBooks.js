97c115eec0b7c4c591a375d551a54634de5fe3eb
Joey Davidson <j.davidson18@gmail.com>
2021-09-29 13:51:55 -0600

    allBooks brought into CardDisplayBooks, mapped over into card

--- client/src/components/CardDisplayBooks/CardDisplayBooks.js
+++ client/src/components/CardDisplayBooks/CardDisplayBooks.js
@@ -1,10 +1,23 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
+import React, { useEffect, useContext } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import Link from "react-router-dom/Link";
+import RecipeContext from "../../utils/RecipeContext";
 
 export default function CardDisplayBooks() {
+  const { bookRecipes, ingredRecipes, courseRecipes, currRecipe, allBooks } =
+    useContext(RecipeContext);
+
+  let booksArr = [];
+
+  useEffect(() => {}, []);
+
   return (
-    <Card>
-      <Card.Body>This is some text within a card body.</Card.Body>
-    </Card>
+    <ListGroup>
+      <ListGroup.Item>SELECT BY BOOK</ListGroup.Item>
+
+      {allBooks[0].map((book) => {
+        return <ListGroup.Item>{book}</ListGroup.Item>;
+      })}
+    </ListGroup>
   );
 }
