6a7e3177f1f96068c23e45e94e5e04f952d3dbd7
Joey Davidson <j.davidson18@gmail.com>
2021-12-12 08:58:23 -0700

    burger nav functional, no transition though - need

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 import MenuIcon from "@mui/icons-material/Menu";
 
 function Header() {
-  const [burgerStatus, setBurgerStatus] = useState(true);
+  const [burgerStatus, setBurgerStatus] = useState(false);
 
   const burgerTrue = burgerStatus
     ? styles.burgerNavTrue
@@ -12,12 +12,22 @@ function Header() {
     <>
       <div className={styles["header__container"]}>
         <div className={styles["header__left"]}>
+          {/* className = burgerNavTrue or burgerNavFalse */}
           <div className={`${burgerTrue}`}>
             <div className={styles["closeWrapper"]}>
-              <div className={styles["customClose"]}>
+              <div
+                className={styles["customClose"]}
+                onClick={() => setBurgerStatus(false)}
+              >
                 <i class="far fa-window-close"></i>
               </div>
             </div>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
           </div>
           <aside className={styles["header__customMenu"]}>
             <i class="fas fa-bars" onClick={() => setBurgerStatus(true)} />
