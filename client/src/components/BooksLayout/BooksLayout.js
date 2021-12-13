ec02fe19951c24ba55238d3b45680dc99ebdc47c
Joey Davidson <j.davidson18@gmail.com>
2021-12-13 09:43:55 -0700

    book grid displayed successfully, queries working, styled, with links

--- client/src/components/BooksLayout/BooksLayout.js
+++ client/src/components/BooksLayout/BooksLayout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./BooksLayout.module.css";
+import styled from "styled-components";
 
 import RecipeContext from "../../utils/RecipeContext";
 import { useContext } from "react";
 import { allBooks } from "../../AllBooks";
+import { Link } from "react-router-dom";
 
 function BooksLayout() {
   const { allTitles } = useContext(RecipeContext);
@@ -12,15 +14,18 @@ function BooksLayout() {
 
   return (
     <>
-      <h4 className={styles["header"]}>Select Book for Recipes</h4>
+      <h5 className={styles["header"]}>Recipes by Book</h5>
       <div className={styles["booksLayout__container"]}>
         {allBooks.map(({ title, cName, img }, index) => {
           return (
             <aside className={styles["bookData"]} key={index}>
-              <img src={img} alt="book cover" className={styles["bookCover"]} />
-              <span className={styles["tempImage"]}>
-                <span>{title}</span>
-              </span>
+              <Link to={`/book/${cName}`} className={styles["bookLink"]}>
+                <BookCover img={img} />
+                {/* <div className={styles["bookCover"]} /> */}
+                <span className={styles["tempImage"]}>
+                  <span>{title}</span>
+                </span>
+              </Link>
             </aside>
           );
         })}
@@ -30,3 +35,12 @@ function BooksLayout() {
 }
 
 export default BooksLayout;
+
+const BookCover = styled.img`
+  background-image: ${(props) => `url('${props.img}')`};
+  height: 190px;
+  width: 100px;
+  border-radius: 3px;
+  background-position: center;
+  background-size: cover;
+`;
