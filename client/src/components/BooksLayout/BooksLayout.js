5861b67d1ed995897ec729d53b5060268cdee80f
Joey Davidson <j.davidson18@gmail.com>
2021-12-14 16:47:07 -0700

    BookLayout grid responsive with queries

--- client/src/components/BooksLayout/BooksLayout.js
+++ client/src/components/BooksLayout/BooksLayout.js
@@ -41,8 +41,11 @@ export default BooksLayout;
 const MainBook = styled.div`
   background-image: ${(props) => `url('${props.background}')`};
   position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
   /* margin: 10px 40px; */
-  margin-top: 10px;
+  margin-top: 435px;
 
   background-repeat: no-repeat;
   background-size: cover;
@@ -50,17 +53,19 @@ const MainBook = styled.div`
   z-index: -1;
   /* height: 730px; */
   height: 650px;
-  width: 1264px;
+  width: 100%;
+  margin-right: 100px;
 
   @media (max-width: 768px) {
-
     height: 1075px;
-    width: 484px;
-    border-radius: 5px
-/* 
-    display: grid;
-    grid-template-columns: repeat(4, 140px); */
-
+    margin-top: 535px;
+    border-radius: 5px;
+  }
+  /* @media (max-width: 1276px) {
+    height: 1075px;
+    width: 1235px;
+    border-radius: 5px;
+  } */
 `;
 
 const BookCover = styled.div`
