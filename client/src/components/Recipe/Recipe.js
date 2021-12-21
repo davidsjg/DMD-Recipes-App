c492abba3b5f9d1e131c5c7d3a7445f64ad2c2fa
Joey Davidson <j.davidson18@gmail.com>
2021-12-21 12:09:02 -0500

    layout for recipe page done

--- client/src/components/Recipe/Recipe.js
+++ client/src/components/Recipe/Recipe.js
@@ -1,54 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import styles from "./Recipe.module.css";
 import API from "../../utils/API";
 import { useDispatch, useSelector } from "react-redux";
+import { allBooks } from "../../AllBooks";
 
 function Recipe() {
   const params = useParams();
   const dispatch = useDispatch();
   const recipe = useSelector((state) => state.recipe.recipe);
   const [currRecipe, setCurrRecipe] = useState();
+  const [currBook, setCurrBook] = useState();
+
   useEffect(() => {
     API.singleQuery(params.recipe, "recipe").then((data) => {
       console.log(data.data);
       dispatch({ type: "recipe/recipeSelected", payload: data.data });
       setCurrRecipe(data.data[0]);
     });
+
+    let tempBook = allBooks.find((book) => (book.title = params));
+    setCurrBook(tempBook);
   }, []);
 
   const handleClick = () => {
-    console.log(currRecipe);
+    console.log(currBook);
   };
 
   return (
     <>
-      <MainImage></MainImage>
-      <h3>{currRecipe?.name}</h3>
+      {/* background image */}
+      <div className={styles["mainImage"]}>
+        <div className={styles["leftContent"]}>
+          <div className={styles["recipeTitle"]}>
+            {/* <button onClick={handleClick}>sup</button> */}
+            {/* <h1>{currRecipe?.name}</h1> */}
+          </div>
+        </div>
+
+        {/* lemon recipe image */}
+        <div className={styles["recipeImg"]}>
+          {/* recipe contents */}
+          <div className={styles["recipe"]}>
+            <h2>{currRecipe?.name}</h2>
+            <span className={styles["recipeUnderline"]}>
+              <hr styles={{ color: "darkred" }} />
+            </span>
+          </div>
+        </div>
+        <div className={styles["bookCover"]}>
+          <BookCover cover={currBook?.img} />
+        </div>
+      </div>
     </>
   );
 }
 
 export default Recipe;
 
-const MainImage = styled.div`
-  /* background-image: url("https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1453&q=80"); */
-  background-image: url("https://images.unsplash.com/photo-1495195134817-aeb325a55b65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80");
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  margin-top: 74px;
-  margin-left: 7px;
-  margin-right: 7px;
-  background-repeat: no-repeat;
+const BookCover = styled.div`
+  background-image: ${(props) => `url('${props.cover}')`};
   background-size: cover;
-  z-index: -1;
-  height: 580px;
-
-  @media (max-width: 768px) {
-    height: 1075px;
-    margin-top: 535px;
-    border-radius: 5px;
-  }
+  width: 150px;
+  height: 250px;
+  border-radius: 8px;
 `;
